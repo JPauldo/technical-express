46e9c9512081b3b9512a00852a160ff0231b39fb
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2022-12-23 21:02:55 -0500

    Initial starter code

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const { User } = require('../models');
+
+router.get('/', async (req, res) => {
+  try {} 
+  catch (err) {}
+});
+
+router.get('/login', (req, res) => {});
+
+module.exports = router;
