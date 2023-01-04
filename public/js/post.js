751b8e606aa05e3ec86023dae605353381ba6176
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-03 23:10:56 -0500

    Added scripts for modal

---
+++ public/js/post.js
@@ -0,0 +1,24 @@
+const postFormHandler = async (event) => {
+  event.preventDefault();
+
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace(`/profile/${response.id}`);
+    } else {
+      alert('Failed to log in.');
+    }
+  }
+};
+
+document
+.querySelector('.post-form')
+.addEventListener('submit', postFormHandler);
