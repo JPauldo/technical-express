b271bd66bb363b38bc900a5b0b42e21ede5707bb
KingdomSeeker328 <30511892+KingdomSeeker328@users.noreply.github.com>
2023-01-21 21:41:34 -0500

    Added datetime formatter

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,1 +1,10 @@
-module.exports = {};
+module.exports = {
+  format_time: (date) => {
+    const options = { hour: "2-digit", minute: "2-digit" };
+    return date.toLocaleTimeString(undefined, options);
+  },
+  format_date: (date) => {
+    const options = { day: "numeric", month: "short", year: "numeric" };
+    return date.toLocaleDateString(undefined, options);
+  }
+};
